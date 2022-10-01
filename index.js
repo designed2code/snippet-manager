c22858fe9a4f806c330ceaec4d86edd50b10b2d6
Sachin Yadav <sachin.yadav@csinfocomm.com>
2022-10-01 18:42:45 +0530

    Basic Express Server setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// Setup Express Server
+app.listen(5000, (req, res) => {
+  console.log(`Server Started on PORT 5000`);
+});
