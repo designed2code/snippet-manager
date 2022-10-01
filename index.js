8083f99fb692d743736f45b67b709fb65caaee7d
Sachin Yadav <sachin.yadav@csinfocomm.com>
2022-10-01 18:55:18 +0530

    nodemon setup with test route

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const mongoose = require('mongoose');
 app.listen(5000, (req, res) => {
   console.log(`Server Started on PORT 5000`);
 });
+
+app.get('/test', (req, res) => {
+  res.send('Hello..');
+});
